c811f9964f8505a0e08ab68e50ad9237b4d8a02b
Brian Hartford <bhartford419@gmail.com>
2024-05-25 10:53:18 -0400

    Stop nginx instead of reload

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ mkdir -p /etc/nginx/sites-available
 mkdir -p /etc/nginx/sites-enabled
 
 # Use Certbot to obtain SSL certificates without interactive prompts
-certbot certonly --nginx -d $DOMAIN --agree-tos --email bh419@protonmail.com --non-interactive
+certbot certonly --nginx -d $DOMAIN --agree-tos --email valid-email@example.com --non-interactive
 
 # Replace the placeholder in the Nginx configuration template with the actual domain
 sed "s/DOMAIN_NAME/$DOMAIN/g" /etc/nginx/nginx.conf.template > /etc/nginx/sites-available/$DOMAIN.conf
@@ -28,13 +28,8 @@ fi
 # Test the Nginx configuration
 nginx -t
 
-# Reload Nginx to apply the new configuration
-if [ $? -eq 0 ]; then
-    nginx -s reload
-else
-    echo "Nginx configuration test failed, not reloading."
-    exit 1
-fi
+# Stop Nginx to free up the ports
+nginx -s stop
 
 # Start Nginx in the foreground
 nginx -g 'daemon off;'
